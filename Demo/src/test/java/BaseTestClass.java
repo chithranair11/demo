29a77db058a69557d7fe916c338fa5b4d52b8bce
Chithra.Nair <Chithra.Nair@PT-CTL-055.Pratian.com>
2020-03-09 14:19:27 +0530

    added

--- Demo/src/test/java/BaseTestClass.java
+++ Demo/src/test/java/BaseTestClass.java
@@ -1,7 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTestClass {
 	
-	public Webdriver driver;
+	public WebDriver driver;
+	private final String chromepath=".\\src\\main\\resources\\chromedriver.exe";
+	
+	@BeforeMethod
+	private void setUp(){
+		
+		System.setProperty("webdriver.chrome.driver", chromepath);
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+	
+		
+		
+	}
 	
 
 }
